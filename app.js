ce5c2d928652001b5519b86ea91a78ddd2a453f0
kiyosakitaisei <kiyotai06@gmail.com>
2024-10-16 15:10:10 +0900

    docs:レビューの修正対応

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ console.log(greet);
 
 
 // Q2
-let languages = ['JavaScript', 'PHP', 'RUdy', 'Python','Go'];
+let languages = ['JavaScript', 'PHP', 'RUdy', 'Python', 'Go'];
 
 console.log('私の好きな言語は' + languages[0] + 'です。次は' + languages[3] + 'を勉強してみたいです。');
 
@@ -45,7 +45,7 @@ console.log(playerList[1].favorites[1])
 
 
 // Q5
-console.log((playerList[0].age + playerList[1].age + playerList[2].age) / 3)
+console.log((playerList[0].age + playerList[1].age + playerList[2].age) / playerList.length)
 
 
 // Q6
@@ -53,7 +53,7 @@ console.log((playerList[0].age + playerList[1].age + playerList[2].age) / 3)
 function sayHello() {
   console.log('Hello');
 }
-sayHello(); 
+sayHello();
 
 
 // 変数 sayWorld に World とコンソールに出力する無名関数を代入し、実行してください。
@@ -66,69 +66,50 @@ sayWorld();
 
 // Q7
 // Q3 で定義した変数 user に '2000-09-27'を値とする birthday プロパティを追加してください。
-user = {
-  birthday: '2000-09-27',
-};
-console.log(user.birthday);
+user.birthday = '2000-09-27';
 
 
 // Q3 で定義した変数 user に Hello！とコンソールに出力するメソッド sayHello を追加し、実行してください。
-user = {
-    sayHello: function() {
-    console.log('Hello！');
-  }
-};
+user.sayHello = function () {
+  console.log('Hello！');
+}
+
 user.sayHello();
 
 
 // Q8
-// 2 つの引数 x, y の和をコンソールに出力する add メソッドを定義し、7 になるように実行して下さい。
 let calc = {
     add: function(x, y) {
     console.log(x + y);
+  },
+
+    subtract: function(x, y) {
+    console.log(x - y);
+  },
+
+    multiply: function(x, y) {
+    console.log(x * y);
+  },
+
+    divide: function(x, y) {
+    console.log(x / y);
   }
 };
 
+
+
 calc.add(3,4);
-
-
-// 2 つの引数 x, y の差をコンソールに出力する subtract メソッドを定義し、10 になるように実行して下さい。
-calc = {
-  subtract: function(x, y) {
-  console.log(x - y);
-}
-};
-
 calc.subtract(11,1);
-
-
-// 2 つの引数 x, y の積をコンソールに出力する multiply メソッドを定義し、49 になるように実行して下さい。
-calc = {
-  multiply: function(x, y) {
-  console.log(x * y);
-}
-};
-
 calc.multiply(7,7);
-
-
-// 2 つの引数 x, y の商をコンソールに出力する divide メソッドを定義し、5 になるように実行して下さい。
-calc = {
-  divide: function(x, y) {
-  console.log(x / y);
-}
-};
-
 calc.divide(25,5);
 
 
 // Q9
 function remainder(x,y) {
-  let division = x % y;
-  return division;
+  return x % y;
 }
 
-console.log('5 を 3 で割った余りは' + remainder(5,3) + 'です'); 
+console.log('5 を 3 で割った余りは' + remainder(5,3) + 'です');
 
 
 // Q10
@@ -148,20 +129,19 @@ console.log('random => ', random);
 
 
 // Q2
-setTimeout(() => {
+setTimeout(function() {
   console.log("Hello World!");
 }, 3000);
 
-
 // Q3
-let num = 1;
+let num = 0;
 if (num > 0) {
   console.log('num is greater than 0');
 }
 else if (num < 0) {
   console.log('num is less than 0');
 }
-else if (num === 0) {
+else {
   console.log('num is 0');
 }
 
@@ -174,31 +154,16 @@ for (let i = 0; i < 100; i++) {
 
 
 // Q5
-// Number であり偶数の時は even
 let mixed = [4, '2', 5, '8', '9', 0, 1];
 
 for (let i = 0; i < mixed.length; i++){
   let element = mixed[i];
 
-  if (typeof element ==='numbers' && element % 2 === 0);{
-    console.log('even')
-  }
-}
-
-// Number であり奇数の時は odd
-for (let i = 0; i < mixed.length; i++){
-  let element = mixed[i];
-
-  if (typeof element ==='numbers' && element % 2 === 1);{
-    console.log('odd')
-  }
-}
-
-// Number 以外の時は not number
-for (let i = 0; i < mixed.length; i++){
-  let element = mixed[i];
-
-  if (typeof element ==='string');{
-    console.log('not number')
+  if (typeof element ==='number' && element % 2 === 0) {
+    console.log('even');
+  } else if (typeof element ==='number' && element % 2 === 1) {
+    console.log('odd');
+  } else {
+    console.log('not number');
   }
 }
